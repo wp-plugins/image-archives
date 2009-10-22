18d459d4e9504cebed7eae70231ada26785ddc1b
coppola00 <coppola00@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-22 23:52:44 +0000

    git-svn-id: https://plugins.svn.wordpress.org/image-archives/tags/0.20@166046 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ image_archives.php
@@ -0,0 +1,336 @@
+<?php
+/*
+ Plugin Name: Image Archives
+ Plugin URI: http://if-music.be/2009/10/15/image-archives/
+ Description: Show images that you searched in your database, and the images are linked to the permalink of posts that the images are attached to.
+ Version: 0.20
+ Author: coppola
+ Author URI: http://if-music.be/
+ */
+ 
+/*  Copyright 2009 coppola (email : coppola@if-music.be)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+class image_archives {
+	
+	var $v_term_id;
+	var $v_order_by;
+	var $v_order;
+	var $v_str;
+	var $v_limit;
+	var $v_img_size;
+	var $v_item;
+	var $v_design;
+	var $v_date_format;
+	var $v_date_show;
+	
+	
+	// shortcode function
+	// shortcode は return を用いても良いが、template tag では使えない。
+	function image_archives_shortcode ( $atts, $content = null ) {
+	
+		extract( shortcode_atts( array(
+			'term_id'		=>	'1',
+			'order_by'		=>	'title',
+			'order'			=>	'ASC',
+			'str'			=>	'%_logo',
+			'limit'			=>	'0,50',
+			'size'			=>	'medium',
+			'design'		=>	'2',
+			'item'			=>	'3',
+			'date_format'	=>	'Y-m-d',
+			'date_show'		=>	'off',
+		), $atts ) );
+		
+		
+		//Substitution
+		
+		//term_id
+		$this->v_term_id	= $term_id;
+		//order_by
+		if ( $order_by == 'title' ) {
+			$this->v_order_by	= 'post_title';
+		} elseif( $order_by == 'date' ) {
+			$this->v_order_by	= 'post_date';
+		} else {
+			return "shortcode atts error. order_by is required to be 'title' or 'date'.";
+		}
+		//order
+		if ( ($order == 'ASC') || ($order == 'DESC') ) {
+			$this->v_order = $order;
+		} else {
+			return "shortcode atts error. order is required to be 'ASC' or 'DESC'.";
+		}
+		//string
+		$this->v_str = $str;
+		//limit
+		$this->v_limit = $limit;
+		//img_size
+		if ( ($size == thumbnail) || ($size == medium) || ($size == large) || ($size == full) ) {
+			$this->v_img_size = $size;
+		} else {
+			return "shortcode atts error. size is required to be 'thumbnail' or 'medium' or 'large' or 'full'.";
+		}
+		//design
+		$this->v_design = intval( $design );
+		//item
+		$this->v_item = intval( $item );
+		if ( $item < 1 ) {
+			return "the number of ITEM is required to be larger than 0.";
+		} elseif( $item > 100 ) {
+			return "the number of ITEM is too big.";
+		}
+		//date format
+		$this->v_date_format = $date_format;
+		//date show
+		if ( ($date_show == on) || ( $date_show == off) ) {
+			$this->v_date_show = $date_show;
+		} else {
+			return "date_show is required to be 'on' or 'off'.";
+		}
+		
+		return $this->image_archives_output();
+	
+	}
+	
+	//template tag function
+	// cannot use "return".
+	function image_archives_template_tag ( $args = '' ) {
+		
+		$default = array (
+			'term_id'		=>	'1',
+			'order_by'		=>	'title',
+			'order'			=>	'ASC',
+			'str'			=>	'%_logo',
+			'limit'			=>	'0,50',
+			'size'			=>	'medium',
+			'design'		=>	'2',
+			'item'			=>	'3',
+			'date_format'	=>	'Y-m-d',
+			'date_show'		=>	'off',
+		);
+		
+		$args = wp_parse_args($args, $default);
+		
+		extract( $args, EXTR_SKIP );
+		
+		
+		//Substitution
+		
+		//term_id
+		$this->v_term_id	= $term_id;
+		//order_by
+		if ( $order_by == 'title' ) {
+			$this->v_order_by	= 'post_title';
+		} elseif( $order_by == 'date' ) {
+			$this->v_order_by	= 'post_date';
+		} else {
+			return "shortcode atts error. order_by is required to be 'title' or 'date'.";
+		}
+		//order
+		if ( ($order == 'ASC') || ($order == 'DESC') ) {
+			$this->v_order = $order;
+		} else {
+			return "shortcode atts error. order is required to be 'ASC' or 'DESC'.";
+		}
+		//string
+		$this->v_str = $str;
+		//limit
+		$this->v_limit = $limit;
+		//img_size
+		if ( ($size == thumbnail) || ($size == medium) || ($size == large) || ($size == full) ) {
+			$this->v_img_size = $size;
+		} else {
+			return "shortcode atts error. size is required to be 'thumbnail' or 'medium' or 'large' or 'full'.";
+		}
+		//design
+		$this->v_design = intval( $design );
+		//item
+		$this->v_item = intval( $item );
+		if ( $item < 1 ) {
+			return "the number of ITEM is required to be larger than 0.";
+		} elseif( $item > 100 ) {
+			return "the number of ITEM is too big.";
+		}
+		//date format
+		$this->v_date_format = $date_format;
+		//date show
+		if ( ($date_show == 'on') || ( $date_show == 'off') ) {
+			$this->v_date_show = $date_show;
+		} else {
+			return "date_show is required to be 'on' or 'off'.";
+		}
+		
+		echo $this->image_archives_output();
+	}
+	
+	
+	
+	function image_archives_query() {
+		
+		global $wpdb;
+		
+		$query =
+			  "SELECT p1.ID as img_post_id, p1.post_parent as parent_id, $wpdb->posts.post_title as post_title, $wpdb->posts.post_date as post_date"
+			. " FROM $wpdb->posts as p1"
+			. " LEFT JOIN $wpdb->term_relationships ON ($wpdb->term_relationships.object_id = p1.post_parent)"
+			. " LEFT JOIN $wpdb->postmeta ON (p1.ID = $wpdb->postmeta.post_id)"
+			. " LEFT JOIN $wpdb->posts ON ($wpdb->posts.ID = p1.post_parent)"
+			. " WHERE p1.post_mime_type LIKE 'image%'"
+			. " AND p1.post_status = 'inherit'"
+			. " AND $wpdb->postmeta.meta_key = '_wp_attached_file'"
+			. " AND p1.post_title LIKE '". $wpdb->escape( $this->v_str ) ."'"
+			. " AND $wpdb->term_relationships.term_taxonomy_id IN (". $wpdb->escape( $this->v_term_id ) .")"
+			. " AND $wpdb->posts.post_status = 'publish'"
+			. " ORDER BY ". $wpdb->escape( $this->v_order_by ) ." ". $wpdb->escape( $this->v_order )
+			. " LIMIT ". $wpdb->escape( $this->v_limit );
+		
+		$query_array = $wpdb->get_results($query, ARRAY_A);
+		// この結果、query_array[ img_post_id / parent_id / post_title / post_date ]
+
+		
+		if(is_array($query_array)) {
+			return $query_array;
+		} else {
+			return false;
+		}
+	}
+	
+	function image_archives_output () {
+		
+		//send query
+		$arr = $this->image_archives_query();
+		
+		if( !$arr ) return "Query Error. Your \"str\"(search strings) may be wrong or your input \"term_id\" don't exsit, or \"limit\" may be wrong.";
+		
+		
+		// OUTPUT
+		
+		if( $this->v_design == 1 ) { 
+			
+			$output = "<table class=\"img_arc\"><tbody>\n";
+			
+			for ($i=0; $arr[$i] !== NULL; $i++) {
+				$img_src = wp_get_attachment_image_src( $arr[$i][img_post_id], $this->v_img_size );
+			
+				$output .= "  <tr>\n"
+				 		.  "    <td class=\"img_arc\">\n"
+						.  "      <div class=\"img_arc_img\"><a class=\"img_arc\" href=\"". get_permalink($arr[$i][parent_id]) ."\"><img class=\"img_arc\" src=\"$img_src[0]\" alt=\"". $arr[$i][post_title] ."\" title=\"". $arr[$i][post_title] ."\" /></a></div>\n"
+						.  "    </td>\n"
+						.  "    <td class=\"img_arc\">\n"
+						.  "      <div class=\"img_arc_text\"><a class=\"img_arc\" href=\"". get_permalink($arr[$i][parent_id]) ."\"><p class=\"img_arc\">". $arr[$i][post_title] ."</p></a>";
+				if ( $this->v_date_show == 'on' ) $output .= "<p class=\"img_src_date\">( ". date( "$this->v_date_format", strtotime($arr[$i][post_date]) ) ." )</p>" ;
+				$output .= "      </div>\n"
+						.  "    </td>\n"
+						.  "  </tr>\n";
+			}
+			
+			$output .= "</tbody></table>\n";
+			
+		} elseif ( $this->v_design == 2) {
+			
+			if ( $this->v_item == 1 ) {
+			
+				$output = "<table class=\"img_arc\"><tbody>\n";
+				
+				for ($i=0; $arr[$i] !== NULL; $i++) {
+					$img_src = wp_get_attachment_image_src( $arr[$i][img_post_id], $this->v_img_size );
+					
+					$output .= "  <tr>\n"
+							.  "    <td class=\"img_arc\">\n"
+							.  "      <div class=\"img_arc_img\"><a class=\"img_arc\" href=\"". get_permalink($arr[$i][parent_id]) ."\"><img class=\"img_arc\" src=\"$img_src[0]\" alt=\"". $arr[$i][post_title] ."\" title=\"". $arr[$i][post_title] ."\" /></a></div>\n"
+							.  "      <div class=\"img_arc_text\"><a class=\"img_arc\" href=\"". get_permalink($arr[$i][parent_id]) ."\"><p class=\"img_arc\">". $arr[$i][post_title] ."</p></a>";
+					if ( $this->v_date_show == 'on' ) $output .= "<p class=\"img_src_date\">( ". date( "$this->v_date_format", strtotime($arr[$i][post_date]) ) ." )</p>" ;
+					$output .= "      </div>\n"
+							.  "    </td>\n"
+							.  "  </tr>\n";
+				}
+				
+				$output .= "</tbody></table>\n";
+				
+			} elseif ( $this->v_item > 1 ) {
+				
+				$output = "<table class=\"img_arc\"><tbody>\n";
+				
+				for ($i=0; $arr[$i] !== NULL; $i++) {
+					$img_src = wp_get_attachment_image_src( $arr[$i][img_post_id], $this->v_img_size );
+					
+					if ( $i % $this->v_item == 0 ) $output .= "  <tr>\n";
+					$output .= "    <td class=\"img_arc\">\n"
+							.  "      <div class=\"img_arc_img\"><a class=\"img_arc\" href=\"". get_permalink($arr[$i][parent_id]) ."\"><img class=\"img_arc\" src=\"$img_src[0]\" alt=\"". $arr[$i][post_title] ."\" title=\"". $arr[$i][post_title] ."\" /></a></div>\n"
+							.  "      <div class=\"img_arc_text\"><a class=\"img_arc\" href=\"". get_permalink($arr[$i][parent_id]) ."\"><p class=\"img_arc\">". $arr[$i][post_title] ."</p></a>";
+					if ( $this->v_date_show == 'on' ) $output .= "<p class=\"img_src_date\">( ". date( "$this->v_date_format", strtotime($arr[$i][post_date]) ) ." )</p>" ;
+					$output	.= "      </div>\n"
+							.  "    </td>\n";
+				
+					if ( $i % $this->item == $this->item - 1 ) $output .= "  </tr>\n";
+				}
+			
+				// $i はループ終了後 +1 されている
+				$i = $i -1;
+				if ( $i % $this->item != $this->item - 1 ) $output .= "  </tr>\n";
+				
+				$output .= "</tbody></table>\n";
+			}
+			
+		} elseif ( $this->v_design == 3 ) {
+			
+			$output = "<div class=\"img_arc\">\n";
+			
+			for ($i=0; $arr[$i] !== NULL; $i++) {
+				$img_src = wp_get_attachment_image_src( $arr[$i][img_post_id], $this->v_img_size );
+				
+				$output .= "  <p class=\"img_arc\"><a class=\"img_arc\" href=\"". get_permalink($arr[$i][parent_id]) ."\"><img class=\"img_arc\" src=\"$img_src[0]\" alt=\"". $arr[$i][post_title] ."\" title=\"". $arr[$i][post_title] ."\" /></a>\n"
+						.  "  <a class=\"img_arc\" href=\"". get_permalink($arr[$i][parent_id]) ."\">". $arr[$i][post_title] ."</a>";
+				if ( $this->v_date_show == 'on' ) $output .= "  ( ". date( "$this->v_date_format", strtotime($arr[$i][post_date]) ) ." )" ;
+				$output .= "</p>\n";
+			}
+			
+			$output .= "</div>\n";
+			
+		} else {
+			return "\"design\" is required to be from 1 to 3." ;
+		}
+		
+
+		
+		return $output;
+	}
+
+}
+
+
+/******************************************************************************
+ * grobal template tag - wp_image_archives()
+ *****************************************************************************/
+
+function wp_image_archives( $args = '' ) {
+	global $image_archives;
+	echo $image_archives->image_archives_template_tag ( $args );
+}
+
+
+
+/******************************************************************************
+ * shortcode - [image_archives]
+ *****************************************************************************/
+
+$image_archives = new image_archives();
+add_shortcode( 'image_archives', array ( $image_archives, 'image_archives_shortcode' ));
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,102 @@
+=== Image Archives ===
+Contributors: coppola00
+Donate link: 
+Tags: image, archive, post list, thumbnail
+Requires at least: 2.0
+Tested up to: 2.8.4
+Stable tag: trunk
+
+Show images that you searched in your database, and the images are linked to the permalink of posts that the images are attached to.
+
+
+== Description ==
+First of all, sorry for my bad English.
+
+Show images that you searched in your database, and the images are linked to the permalink of posts that the images are attached to.
+
+[Sample](http://if-music.be/2009/10/15/image-archives/)
+
+= NOTICE =
+* The images that you want to show is needed to be attached to posts.
+* In order to link to the permalink of a post, its post is required to be "published".
+
+= HOW TO USE =
+Write shortcode "\[image\_archives\]" in the place you want to show the image archives.
+And you can set some attributes.
+
+\[image\_archives *term\_id=?* *ordered\_by=?* *order=?* *str=?* *limit=?* *size=?* *design=?* *item=?* *date_format=?* *date_show=?*\]
+
+Write only necessary attributes. Default settings are below.
+
+You can also use the function of this plugin. In order to use the function, write *wp\_image\_archives* within php code. And in order to use the attributes, write like below.
+
+wp\_image\_archives \('term\_id=9&order=DESC&design=1'\);
+
+
+= Default =
+* term\_id    = 1
+* order\_by    = title
+* order       = ASC
+* str         = %_logo
+* limit       = 0,50
+* size        = medium
+* design      = 2
+* item        = 3
+* date\_format = Y-m-d
+* date\_show   = off
+
+= explanation =
+* **term\_id** requires unique ID(s) of tags or categories. You can use several IDs like 'term\_id=1,3,10'. Numbers only.
+* **order\_by** is  how to order the image list.  You can use "title" or "date".
+* **order** requires a sort type. You can use "ASC" or "DESC". Uppercase only.
+* **str** is a search string. This plugin searches "post\_title"(these are seen in "MEDIA LIBRARY" -> "FILE" or "TITLE") in your wordpress database for the string. This string is required to be SQL LIKE condition string. Please refer to [SQL LIKE condition](http://www.techonthenet.com/sql/like.php).
+* **lmit** is a limit of the images that is shown. Write this attribute like '*start number*,*end number*' . example, 'limit=0,30' . You can also use this like '20,50'.
+* **size** is the size of the images. "thumbnail" or "medium" or "large" or "full".
+* **design** is the type of output design. "1" or "2" at present.
+* **item** is the number of images in a line of a table. This attribute is enabled only when "design=2".
+* **date\_format** is the date format. Please refer to [PHP.net date format](http://php.net/manual/en/function.date.php).
+* **date\_show** is if you want to show posts' date or not. You can use "on" or "off".
+
+You can also change design of table with CSS.  output HTML tags, *table, td, a, img, p,* have a *class="img\_arc"*. *div* before a image have a class="img\_arc\_img", *div* before text have a class="img\_arc\_text". And if you show posts' date, on design=1 and 2, *p* before the post date have a class="img\_arc\_date".
+
+= CSS example =
+
+table.img\_arc { <br />
+&nbsp;&nbsp;border: 0 none;<br />
+}
+
+div.img\_arc\_img, div.img\_arc\_text { <br />
+&nbsp;&nbsp;text-align: center; <br />
+&nbsp;&nbsp;line-height: 1; <br />
+}
+
+p.img\_arc { <br />
+&nbsp;&nbsp;text-decoration: none; <br />
+}
+
+== Installation ==
+
+1. Upload this plugin files to the "/wp-content/plugins/" directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+1. Write shortcode "[image_archives]" in your post. And some attributes are needed.
+
+
+== Screenshot ==
+1. Output designs.
+
+
+== Changelog ==
+
+= 0.20 =
+* Add a template tag of this plugin. *wp\_image\_archives*.
+* Add attributes "limit", "date\_format", "date\_show".
+* Add design=3.
+* Default *term\_id* is changed to 1.
+* Changed class names.
+* Fixed some codes.
+
+= 0.11 =
+* Add a attribute "title" to output img tags.
+
+= 0.10 =
+* Release
