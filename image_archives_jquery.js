9e0c0b4af61b9de2bd7c143a88b83ebf1084bc82
coppola00 <coppola00@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-28 07:11:01 +0000

    git-svn-id: https://plugins.svn.wordpress.org/image-archives/tags/0.690@481384 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ image_archives_jquery.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($){
+	// Accordion
+	$("div.image_archives.accordion").accordion({ header: "h3", autoHeight: false });
+	
+	// Tabs
+	$("div.image_archives.tabs").tabs();
+	
+});
